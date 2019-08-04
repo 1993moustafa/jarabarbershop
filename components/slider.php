055c046252a075ae064f9b45eb0dd628571ad6e5
Moustafa Omar <1993moustafa@gmail.com>
2019-08-04 21:31:30 +0200

    pricing widgets

--- components/slider.php
+++ components/slider.php
@@ -1,11 +1,10 @@
 <div class="slider py-5" style="background-image:url(<?php bloginfo('template_url'); ?>/assets/images/slider.jpg)">
   <div class="container">
     <div class="row">
-      <div class="col-12 col-lg-6">
-        <h1>Being a barber is about taking care of the people.</h1>
+      <div class="col-8 text-center mx-auto">
+        <h1 class="m-0">Being a barber is about taking care of the people.</h1>
         <p class="lead">Rem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
       </div>
-      <div class="col-12 col-lg-6"></div>
     </div>
   </div>
 </div>
